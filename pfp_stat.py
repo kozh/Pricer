8cf004b239c40dda2f7fa1dd5013abb7929d9030
kozh <alkozhanov@gmail.com>
2020-02-03 12:18:01 +0300

    Statistics update
    Добавлены два параметра в расчет статистики - частота точек в году и глубина в годах.

--- pfp_stat.py
+++ pfp_stat.py
@@ -4,50 +4,67 @@ from datetime import datetime
 import random
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from dateutil.relativedelta import relativedelta
 
+def get_hist(BAs, points_in_year, stat_depth):
 
-def ba_scenarios(BAs, simulation_years, n_scenarios, print_statistics = False, points_in_year = 12):
+    BAs = [(x.lower()) for x in BAs]
+    Hist = pd.read_excel('inputs/quotes daily.xlsx', 'quotes', decimal = '.')
+    Hist.columns = Hist.columns.str.lower()
+    Hist = Hist[[(x.lower()) for x in BAs]+['date']]
+    Hist.index = Hist.date
+    Hist.drop('date', 1, inplace = True)
+    Hist.fillna( method ='ffill', inplace = True)
+    Hist = (Hist[Hist.index.dayofweek < 5])
+    Hist = Hist[Hist.index >=  (Hist.index[-1] - relativedelta(years=stat_depth))]
+    bus_days_in_the_period = np.int(np.round(Hist.shape[0]/(stat_depth*points_in_year)))
+    Hist = Hist.iloc[::bus_days_in_the_period,:]
+    return Hist
 
-    days_in_year = 260
-    c01  = {0: 1.9, 1: 4.5, 2:1.4, 3:4, 4:1.7}
+def calc_correlations(BAs, points_in_year, stat_depth):
+
+    BAs = [(x.lower()) for x in BAs]
+
+    Hist = pd.read_excel('inputs/quotes daily.xlsx', 'quotes', decimal = '.')
+    Hist.columns = Hist.columns.str.lower()
+    Hist = Hist[[(x.lower()) for x in BAs]+['date']]
+    Hist.index = Hist.date
+    Hist.drop('date', 1, inplace = True)
+    Hist.fillna( method ='ffill', inplace = True)
+    Hist = (Hist[Hist.index.dayofweek < 5])
+
+    # это полная история. теперь берем срезы:
+
+    cors = np.zeros((len(points_in_year), len(stat_depth)))
+
+    for i, points in enumerate(points_in_year):
+        for j, depth in enumerate(stat_depth):
+            Hist1 = Hist[Hist.index >=  (Hist.index[-1] - relativedelta(years=stat_depth[j]))]
+            #Hist1 = Hist[Hist.index >=  (Hist.index[-1] - relativedelta(years=stat_depth[j]))]
+            bus_days_in_the_period = np.int(np.round(Hist1.shape[0]/(stat_depth[j]*points_in_year[i])))
+            Hist2 = Hist1.iloc[::bus_days_in_the_period,:]
+            Returns = np.log((Hist2/Hist2.shift(1)).astype(np.float32))
+            corr = Returns[BAs].corr()
+            cors[i, j] = corr.iloc[0,1] 
+    return cors
+
+def ba_scenarios(BAs, simulation_years, n_scenarios, print_statistics = False, points_in_year = 12, stat_depth = 3):
 
     BAs = [(x.lower()) for x in BAs]
 
     n_timepoints = points_in_year*simulation_years
-    dt = days_in_year/points_in_year
 
-    Hist = pd.read_excel('inputs/quotes daily.xlsx', 'quotes', decimal = '.')
-    Hist.columns = Hist.columns.str.lower()
-
-    Hist = Hist[[(x.lower()) for x in BAs]+['date']]
-    Hist.index = Hist.date
-    Hist = (Hist[Hist.index.dayofweek <5]).drop('date', 1)
-
+    Hist = get_hist(BAs, points_in_year, stat_depth)
     Returns = np.log((Hist/Hist.shift(1)).astype(np.float32))
 
     RDiv = pd.read_excel('inputs/quotes daily.xlsx', 'details', decimal = '.')
     RDiv.index = [(x.lower()) for x in RDiv.index]
+    
+    mu = (RDiv.loc[BAs])['r']/points_in_year
 
-    mu = (RDiv.loc[BAs])['r+div']/days_in_year
     std = Returns[BAs].std()
     var = Returns[BAs].var()
-    cov = Returns[BAs].cov()
-
-    #corr = np.power(Returns[BAs].corr(),1)
-    corr = (Returns[BAs].dropna()).corr()
-
-    corr2 = corr
-    for i in range(len(BAs)-1):
-        for j in range(i+1,len(BAs)-1):
-            print(i)
-            print(j)
-            print((((Returns[[BAs[i+1],BAs[j+1]]]).dropna()).corr()).iloc[0,1])
-            corr2.iloc[i+1,j+1] = (((Returns[[BAs[i+1],BAs[j+1]]]).dropna()).corr()).iloc[0,1]
-
-    print(corr2)
-
-
-
+    corr = Returns[BAs].corr()
 
     drift = mu - (0.5*var)
 
@@ -56,26 +73,22 @@ def ba_scenarios(BAs, simulation_years, n_scenarios, print_statistics = False, p
 
     pre_returns = np.zeros_like(randoms)
     for i in range(len(mu)):
-        pre_returns[:,:,i] = drift[i]*dt + std[i]*randoms[:,:,i]*(dt**0.5)
+        pre_returns[:,:,i] = drift[i] + std[i]*randoms[:,:,i]
     returns = np.exp(pre_returns.cumsum(axis = 0))
 
     if print_statistics:
-        print('Means: \n' + str(mu*252))      
+        print('Returns: \n' + str(mu*points_in_year))      
         print(' ')
-        print('Sigmas: \n' + str(Returns[BAs].std()*16))
+        print('Sigmas: \n' + str(Returns[BAs].std()*(points_in_year**0.5)))
         print(' ')
         print('Correlations: \n')
         print(corr)
-        print(' ')
-        print('Correlation power: \n')
-        print(np.linalg.det(Returns[BAs].corr()))
     return returns
 
 def rfr(currency, term):
 
     curves = pd.read_excel('inputs/curves.xlsx', 'Sheet1', decimal = '.')
     rfr = curves[term][currency]
-
     return rfr
 
 def check_stat(BAs):
